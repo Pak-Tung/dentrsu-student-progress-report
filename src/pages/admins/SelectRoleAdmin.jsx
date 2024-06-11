31095626ee233e5cd643af9ad68449d23c4f6a34
oddident <oddhahey@gmail.com>
2024-06-11 17:35:18 +0700

    minor change ProfileAdmin

--- src/pages/admins/SelectRoleAdmin.jsx
+++ src/pages/admins/SelectRoleAdmin.jsx
@@ -44,8 +44,15 @@ function SelectRoleAdmin() {
   return (
     <Container fluid>
       <Row className="justify-content-center">
-        <Col className="text-center">
+      <Col md={4}></Col>
+        <Col md={4} className="text-center">
           <h2>Welcome Back!</h2>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+      <Row>
+      <Col md={4}></Col>
+        <Col md={4} className="text-center">
           <img
             src={userPicture}
             alt={`${userName}'s profile`}
@@ -54,16 +61,42 @@ function SelectRoleAdmin() {
             height="100"
           />
           <h3>{userName}</h3>
-          <h5>Please Select Role to Login</h5>
-          <div className="d-grid gap-2">
-            <Button variant="secondary" size="lg" onClick={() => handleSelectRole("admin")}>
-              ADMIN
-            </Button>
-            <Button variant="secondary" size="lg" onClick={() => handleSelectRole("instructor")}>
-              INSTRUCTOR
-            </Button>
-          </div>
         </Col>
+        <Col md={4}></Col>
+      </Row>
+      <br />
+      <Row>
+        <Col className="text-center">
+          <h5>Please Select Role to Login</h5>
+        </Col>
+      </Row>
+      <br />
+      <Row className="justify-content-center">
+        <Col md={4}></Col>
+        <Col className="text-center">
+          <Button
+            variant="secondary"
+            size="lg"
+            onClick={() => handleSelectRole("admin")}
+          >
+            ADMIN
+          </Button>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
+      <br />
+      <Row className="justify-content-center">
+        <Col md={4}></Col>
+        <Col className="text-center">
+          <Button
+            variant="secondary"
+            size="lg"
+            onClick={() => handleSelectRole("instructor")}
+          >
+            INSTRUCTOR
+          </Button>
+        </Col>
+        <Col md={4}></Col>
       </Row>
     </Container>
   );
