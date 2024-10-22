fa16f3a72f08df396c70eaf127ae86b19ca34a81
oddident <oddhahey@gmail.com>
2024-10-22 12:39:29 +0700

    changed link

--- src/features/mainApi.js
+++ src/features/mainApi.js
@@ -2,5 +2,5 @@
 //export const host = 'https://dentrsu-0b0ffade06fd.herokuapp.com';//'http://localhost:3001'; //
 
 export const api = () => {
-    return 'http://localhost:3001'; //'https://dentrsu-0b0ffade06fd.herokuapp.com'; //
+    return 'https://dentrsu-0b0ffade06fd.herokuapp.com'; //'http://localhost:3001'; //
 }
