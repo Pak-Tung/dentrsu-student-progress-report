740c1877eca49019575dc1a04a8fff986eac108f
oddident <oddhahey@gmail.com>
2024-11-24 13:51:42 +0700

    Revert "add red  notification dot to approval status"
    This reverts commit e4e407d73fc2dd8581e911ae3faca7ff3e8bd851.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+// NavbarStudent.js
 import React, { useContext, useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
@@ -7,11 +8,6 @@ import Navbar from "react-bootstrap/Navbar";
 import "../Navbar.css";
 import "../DarkMode.css";
 import { ThemeContext } from "../ThemeContext";
-import {
-  getDivReqByStudentEmail,
-  getCompcaseReqByStudentEmail,
-} from "../features/apiCalls";
-import { Badge } from "react-bootstrap";
 
 function NavbarStudent() {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -20,24 +16,6 @@ function NavbarStudent() {
     const savedUser = Cookies.get("user");
     return savedUser ? JSON.parse(savedUser) : {};
   });
-
-  const [showNotificationDot, setShowNotificationDot] = useState(false);
-  const [showCompletionNotificationDot, setShowCompletionNotificationDot] =
-    useState(false);
-  const [showApprovalNotificationDot, setShowApprovalNotificationDot] =
-    useState(false);
-
-  const divisions = [
-    "oper",
-    "endo",
-    "perio",
-    "prosth",
-    "diag",
-    "radio",
-    "sur",
-    "ortho",
-    "pedo",
-  ];
 
   useEffect(() => {
     const savedUser = Cookies.get("user");
@@ -48,64 +26,6 @@ function NavbarStudent() {
     }
   }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (user.email) {
-          let completedCasesNotificationNeeded = false;
-          let approvalNotificationNeeded = false;
-
-          // Check for requirements needing submission
-          for (const division of divisions) {
-            const reqs = await getDivReqByStudentEmail(user.email, division);
-
-            if (reqs && reqs.length > 0) {
-              for (const req of reqs) {
-                // For "Approval Status" notification dot
-                if (req.isApproved === -1) {
-                  approvalNotificationNeeded = true;
-                }
-              }
-            }
-
-            // Break early if both notifications are needed
-            if (approvalNotificationNeeded) {
-              break;
-            }
-          }
-
-          // Check for completed cases needing approval
-          const completedCases = await getCompcaseReqByStudentEmail(user.email);
-          //console.log("completedCases", completedCases);
-          if (completedCases && completedCases.length > 0) {
-            for (const caseItem of completedCases) {
-              //console.log("caseItem", caseItem.isApproved);
-              if (caseItem.isApproved === -1) {
-                completedCasesNotificationNeeded = true;
-                break;
-              }
-
-              // Break early if both notifications are needed
-              if (completedCasesNotificationNeeded) {
-                break;
-              }
-            }
-          }
-
-          setShowApprovalNotificationDot(
-            approvalNotificationNeeded || completedCasesNotificationNeeded
-          );
-          setShowNotificationDot(approvalNotificationNeeded);
-          setShowCompletionNotificationDot(completedCasesNotificationNeeded);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [user.email]);
-
   return (
     <Navbar
       expand="md"
@@ -114,9 +34,7 @@ function NavbarStudent() {
       <div className="logo-navbar-div">
         <Navbar.Brand href="/">
           <img
-            src={
-              theme === "light" ? "/logo_navbar.png" : "/logo_navbar_dark.png"
-            }
+            src={theme === "light"?"/logo_navbar.png":"/logo_navbar_dark.png"}
             width="30"
             height="30"
             className="d-inline-block align-top"
@@ -132,20 +50,13 @@ function NavbarStudent() {
               All My Patients
             </NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title="Submission" id="nav-submit" className="flex-fill">
-            <NavDropdown.Item href="/reqSubmit" className="position-relative">
-              Requirements
-              {showNotificationDot && (
-                <Badge
-                  pill
-                  bg="danger"
-                  className="position-absolute top-0 start-100 translate-middle p-1 border border-light rounded-circle"
-                  style={{ zIndex: 1 }}
-                >
-                  <span className="visually-hidden">New alerts</span>
-                </Badge>
-              )}
-            </NavDropdown.Item>
+          <NavDropdown
+            title="Submission"
+            id="nav-submit"
+            className="flex-fill"
+            //disabled
+          >
+            <NavDropdown.Item href="/reqSubmit">Requirements</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/compSubmit">
               Completed Cases
@@ -155,57 +66,28 @@ function NavbarStudent() {
             title="Approval Status"
             id="nav-status"
             className="flex-fill"
+            //disabled
           >
-            <NavDropdown.Item href="/reqStatus" className="position-relative">
-              <span>
-                Requirements
-                {showApprovalNotificationDot && (
-                  <Badge
-                    pill
-                    bg="danger"
-                    className="position-absolute top-0 start-100 translate-middle p-1 border border-light rounded-circle"
-                    style={{ zIndex: 1 }}
-                  >
-                    <span className="visually-hidden">New alerts</span>
-                  </Badge>
-                )}
-              </span>
+            <NavDropdown.Item href="/reqStatus">
+              Requirements
             </NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/compReport">
-              <span>
-                Completed Cases
-                {showCompletionNotificationDot && (
-                  <Badge
-                    pill
-                    bg="danger"
-                    className="position-absolute top-3 start-100 translate-middle p-1 border border-light rounded-circle"
-                    style={{ zIndex: 1 }}
-                  >
-                    <span className="visually-hidden">New alerts</span>
-                  </Badge>
-                )}
-              </span>
+              Completed Cases
             </NavDropdown.Item>
           </NavDropdown>
-          {showApprovalNotificationDot && (
-            <Badge
-              pill
-              bg="danger"
-              className="position-absolute top-1 start-56 translate-middle p-1 border border-light rounded-circle"
-              style={{ zIndex: 1 }}
-            >
-              <span className="visually-hidden">New alerts</span>
-            </Badge>
-          )}
           <NavDropdown title="Overview" id="nav-overview" className="flex-fill">
-            <NavDropdown.Item href="/overview">Requirements</NavDropdown.Item>
+            <NavDropdown.Item href="/overview">
+              Requirements
+            </NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/compOverview">
               Completed Cases
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/checkStatus">Radar Chart</NavDropdown.Item>
+            <NavDropdown.Item href="/checkStatus">
+              Radar Chart
+            </NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Profile" id="nav-profile" className="flex-fill">
             <NavDropdown.Item href="/">Profile</NavDropdown.Item>
